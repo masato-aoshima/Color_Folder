6b0b0e4252dbf8e79b94258a4b0b47a016880fa3
Aoshima <>
2021-01-30 16:16:05 +0900

    ノートのモデルクラスを作成

---
+++ lib/note_list/note_model.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sort_note/model/note.dart';
+import 'package:sort_note/repository/database.dart';
+
+// 2. モデルクラスで、ChangeNotifierを継承する
+class NoteModel extends ChangeNotifier {
+  var _notes = List<Note>();
+
+  List<Note> get notes => _notes;
+
+  void getNotes(int folderId) async {
+    _notes = await DBProvider.db.getNotesInFolder(folderId);
+    notifyListeners();
+  }
+
+  void addNotes(Note note) async {
+    await DBProvider.db.insertNote(note);
+    _notes = await DBProvider.db.getNotesInFolder(note.folderId);
+    notifyListeners();
+  }
+
+  void deleteNote(int id, int folderId) async {
+    await DBProvider.db.deleteNote(id.toString());
+    _notes = await DBProvider.db.getNotesInFolder(folderId);
+    notifyListeners();
+  }
+
+  void upDateNote(Note note) async {
+    await DBProvider.db.updateNote(note);
+    _notes = await DBProvider.db.getNotesInFolder(note.folderId);
+    notifyListeners();
+  }
+}
