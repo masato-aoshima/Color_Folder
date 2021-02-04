a12dae986cd88c458fc5820f4b9380114aeb26fb
Aoc4456 <sizu44567789@gmail.com>
2021-02-04 22:29:08 +0900

    メモ一覧をListTileに変更

--- lib/component/list_item/list_item_note.dart
+++ lib/component/list_item/list_item_note.dart
@@ -10,28 +10,19 @@ class ListItemNote extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 80,
-      child: Card(
-        margin: const EdgeInsets.all(8.0),
-        elevation: 4.0,
-        child: Center(
-          child: ListTile(
-            title: Text(
-              text.split("\n").first,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              maxLines: 1,
-            ),
-            trailing: Icon(Icons.navigate_next),
-            onTap: () {
-              onTapCallback();
-            },
-            onLongPress: () {
-              onLongPressCallback();
-            },
-          ),
-        ),
+    return ListTile(
+      title: Text(
+        text.split("\n").first,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        maxLines: 1,
       ),
+      trailing: Icon(Icons.navigate_next),
+      onTap: () {
+        onTapCallback();
+      },
+      onLongPress: () {
+        onLongPressCallback();
+      },
     );
   }
 }
--- lib/screen/note_list/notes_page.dart
+++ lib/screen/note_list/notes_page.dart
@@ -52,45 +52,48 @@ class NotePage extends HookWidget {
   Widget getListViewWithEmptyMessage(
       BuildContext context, List<Note> notes, NoteModel provider) {
     if (notes.length > 0) {
-      return ListView(
-        children: notes
-            .map((note) => ListItemNote(
-                  text: note.text,
-                  onTapCallback: () async {
-                    // メモ編集ページに移動
-                    await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => NoteAddEditPage(
-                              note.id, note.text, folderId, folderName),
-                        ));
-                  },
-                  onLongPressCallback: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return MoveOrDeleteDialog(
-                            noteText: note.text,
-                            moveFunction: () async {
-                              await Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          MoveAnotherFolderPage(
-                                              note.id, note.folderId, null),
-                                      fullscreenDialog: true));
-                              Navigator.pop(context);
-                            },
-                            deleteFunction: () async {
-                              await provider.deleteNote(note.id,
-                                  note.folderId); //TODO ここで残りが0件だとワーニングが出る
-                              Navigator.pop(context);
-                            },
-                          );
-                        });
-                  },
-                ))
-            .toList(),
+      return ListView.separated(
+        itemCount: notes.length,
+        separatorBuilder: (BuildContext context, int index) =>
+            Divider(color: Colors.grey),
+        itemBuilder: (BuildContext context, int index) {
+          final note = notes[index];
+          return ListItemNote(
+            text: note.text,
+            onTapCallback: () async {
+              // メモ編集ページに移動
+              await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => NoteAddEditPage(
+                        note.id, note.text, folderId, folderName),
+                  ));
+            },
+            onLongPressCallback: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return MoveOrDeleteDialog(
+                      noteText: note.text,
+                      moveFunction: () async {
+                        await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MoveAnotherFolderPage(
+                                    note.id, note.folderId, null),
+                                fullscreenDialog: true));
+                        Navigator.pop(context);
+                      },
+                      deleteFunction: () async {
+                        await provider.deleteNote(
+                            note.id, note.folderId); //TODO ここで残りが0件だとワーニングが出る
+                        Navigator.pop(context);
+                      },
+                    );
+                  });
+            },
+          );
+        },
       );
     } else {
       return Center(
