76c53153cd72d2399e9bec3a9ff41a5e211a6226
Aoc4456 <sizu44567789@gmail.com>
2021-02-15 23:25:35 +0900

    フォルダ詳細画面　横回転対応

--- lib/screen/folder_detail/folder_detail_page.dart
+++ lib/screen/folder_detail/folder_detail_page.dart
@@ -29,119 +29,207 @@ class FolderDetailPage extends HookWidget {
         return Future.value(false);
       },
       child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            folder == null ? '新規作成' : 'フォルダー編集',
-            style: TextStyle(fontWeight: FontWeight.bold),
+          appBar: AppBar(
+            title: Text(
+              folder == null ? '新規作成' : 'フォルダー編集',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            actions: [
+              Visibility(
+                visible: folder != null,
+                child: IconButton(
+                    icon: Icon(
+                      Icons.delete_forever,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (_) {
+                            return AlertDialog(
+                              title: Text('フォルダーを削除しますか？'),
+                              content: Text(
+                                  'このフォルダーと、フォルダー内のすべてのノートが削除されます。この操作は取り消せません。'),
+                              actions: [
+                                // ボタン領域
+                                FlatButton(
+                                  child: Text(
+                                    "キャンセル",
+                                  ),
+                                  onPressed: () => Navigator.pop(context),
+                                ),
+                                FlatButton(
+                                  child: Text(
+                                    "削除",
+                                    style: TextStyle(color: Colors.red),
+                                  ),
+                                  onPressed: () async {
+                                    await provider.deleteFolder(folder);
+                                    Navigator.pop(context);
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                              ],
+                            );
+                          });
+                    }),
+              )
+            ],
           ),
-          actions: [
-            Visibility(
-              visible: folder != null,
-              child: IconButton(
-                  icon: Icon(
-                    Icons.delete_forever,
-                    color: Colors.white,
-                    size: 30,
-                  ),
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (_) {
-                          return AlertDialog(
-                            title: Text('フォルダーを削除しますか？'),
-                            content: Text(
-                                'このフォルダーと、フォルダー内のすべてのノートが削除されます。この操作は取り消せません。'),
-                            actions: [
-                              // ボタン領域
-                              FlatButton(
-                                child: Text(
-                                  "キャンセル",
+          body: OrientationBuilder(
+            builder: (context, orientation) {
+              if (orientation == Orientation.portrait) {
+                return Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 0),
+                  child: Center(
+                      child: Column(
+                    children: [
+                      Hero(
+                          tag: folder == null
+                              ? ''
+                              : 'folderSmallIcon${folder.id}',
+                          child: FolderSmallIcon(
+                            color: provider.color,
+                            size: 200,
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 0, horizontal: 20),
+                        child: TextFormField(
+                          controller: myController,
+                          textAlign: TextAlign.center,
+                          onChanged: provider.changeText,
+                          maxLines: 1,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      ColorPicker(
+                        pickersEnabled: {
+                          ColorPickerType.both: false,
+                          ColorPickerType.primary: true,
+                          ColorPickerType.accent: false,
+                          ColorPickerType.bw: false,
+                          ColorPickerType.custom: false,
+                          ColorPickerType.wheel: false
+                        },
+                        enableShadesSelection: false,
+                        hasBorder: true,
+                        padding: EdgeInsets.all(20),
+                        color: provider.color,
+                        onColorChanged: (Color color) {
+                          print(colorToString(color));
+                          provider.selectColor(color);
+                        },
+                        heading: Text(
+                          'フォルダの色を変更できます',
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                      ),
+                      RaisedButton(
+                        child: const Text(
+                          '保存',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () async {
+                          if (provider.inputText == null ||
+                              provider.inputText.isEmpty) {
+                            showEmptyTextToast();
+                          } else {
+                            await provider.onTapSave();
+                            provider.clear();
+                            Navigator.pop(context);
+                          }
+                        },
+                        color: Color(0xff1995AD),
+                      ),
+                    ],
+                  )),
+                );
+              } else {
+                // 横画面
+                return Container(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 12, horizontal: 0),
+                    child: Row(
+                      children: [
+                        Flexible(
+                          flex: 1,
+                          child: Column(
+                            children: [
+                              Hero(
+                                  tag: folder == null
+                                      ? ''
+                                      : 'folderSmallIcon${folder.id}',
+                                  child: FolderSmallIcon(
+                                    color: provider.color,
+                                    size: 200,
+                                  )),
+                              Container(
+                                width: double.infinity,
+                                child: TextFormField(
+                                  controller: myController,
+                                  textAlign: TextAlign.center,
+                                  onChanged: provider.changeText,
+                                  maxLines: 1,
+                                  style: TextStyle(fontSize: 20),
                                 ),
-                                onPressed: () => Navigator.pop(context),
-                              ),
-                              FlatButton(
-                                child: Text(
-                                  "削除",
-                                  style: TextStyle(color: Colors.red),
-                                ),
-                                onPressed: () async {
-                                  await provider.deleteFolder(folder);
-                                  Navigator.pop(context);
-                                  Navigator.pop(context);
-                                },
                               ),
                             ],
-                          );
-                        });
-                  }),
-            )
-          ],
-        ),
-        body: Container(
-          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 0),
-          child: Center(
-              child: Column(
-            children: [
-              Hero(
-                  tag: folder == null ? '' : 'folderSmallIcon${folder.id}',
-                  child: FolderSmallIcon(
-                    color: provider.color,
-                    size: 200,
-                  )),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
-                child: TextFormField(
-                  controller: myController,
-                  textAlign: TextAlign.center,
-                  onChanged: provider.changeText,
-                  maxLines: 1,
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              ColorPicker(
-                pickersEnabled: {
-                  ColorPickerType.both: false,
-                  ColorPickerType.primary: true,
-                  ColorPickerType.accent: false,
-                  ColorPickerType.bw: false,
-                  ColorPickerType.custom: false,
-                  ColorPickerType.wheel: false
-                },
-                enableShadesSelection: false,
-                hasBorder: true,
-                padding: EdgeInsets.all(20),
-                color: provider.color,
-                onColorChanged: (Color color) {
-                  print(colorToString(color));
-                  provider.selectColor(color);
-                },
-                heading: Text(
-                  'フォルダの色を変更できます',
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-              ),
-              RaisedButton(
-                child: const Text(
-                  '保存',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () async {
-                  if (provider.inputText == null ||
-                      provider.inputText.isEmpty) {
-                    showEmptyTextToast();
-                  } else {
-                    await provider.onTapSave();
-                    provider.clear();
-                    Navigator.pop(context);
-                  }
-                },
-                color: Color(0xff1995AD),
-              ),
-            ],
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: Column(
+                            children: [
+                              ColorPicker(
+                                pickersEnabled: {
+                                  ColorPickerType.both: false,
+                                  ColorPickerType.primary: true,
+                                  ColorPickerType.accent: false,
+                                  ColorPickerType.bw: false,
+                                  ColorPickerType.custom: false,
+                                  ColorPickerType.wheel: false
+                                },
+                                enableShadesSelection: false,
+                                hasBorder: true,
+                                padding: EdgeInsets.all(20),
+                                color: provider.color,
+                                onColorChanged: (Color color) {
+                                  print(colorToString(color));
+                                  provider.selectColor(color);
+                                },
+                                heading: Text(
+                                  'フォルダの色を変更できます',
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                              ),
+                              RaisedButton(
+                                child: const Text(
+                                  '保存',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                onPressed: () async {
+                                  if (provider.inputText == null ||
+                                      provider.inputText.isEmpty) {
+                                    showEmptyTextToast();
+                                  } else {
+                                    await provider.onTapSave();
+                                    provider.clear();
+                                    Navigator.pop(context);
+                                  }
+                                },
+                                color: Color(0xff1995AD),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ));
+              }
+            },
           )),
-        ),
-      ),
     );
   }
 
