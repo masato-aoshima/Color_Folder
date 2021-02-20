4263907f8223bd8fa650bdc50e7749acd1424705
Aoc4456 <sizu44567789@gmail.com>
2021-02-20 18:35:22 +0900

    設定の見出し用のTextを作成

--- lib/screen/settings/settings_page.dart
+++ lib/screen/settings/settings_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sort_note/component/text/text_setting_heading.dart';
 import 'package:sort_note/screen/settings/settings_model.dart';
+import 'package:sort_note/util/color.dart';
 
 // 3. Providerモデルクラスをグローバル定数に宣言
 final settingsProvider = ChangeNotifierProvider((ref) => SettingsModel());
@@ -12,15 +14,19 @@ class SettingsPage extends HookWidget {
   Widget build(BuildContext context) {
     final provider = useProvider(settingsProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          '設定',
-          style: TextStyle(fontWeight: FontWeight.bold),
+        appBar: AppBar(
+          title: Text(
+            '設定',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
-      ),
-      body: Center(
-        child: Text('設定画面です'),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              TextSettingHeading('テーマ'),
+            ],
+          ),
+        ));
   }
 }
