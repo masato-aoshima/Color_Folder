d36397c7a77d217643bdc141466e05dc6fa8a96d
Aoc4456 <sizu44567789@gmail.com>
2021-02-20 20:18:21 +0900

    appをDynamicThemeでラップ

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/all.dart';
 import 'package:sort_note/screen/folder_list/folder_list_page.dart';
@@ -12,26 +13,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '並び替え　メモ帳',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Color(0xff1995AD),
-        scaffoldBackgroundColor: Color(0xffF1F1F2),
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-            backgroundColor: Color(0xff1995AD), // TODO
-            foregroundColor: getWhiteOrBlack(Color(0xff1995AD))), // TODO
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        pageTransitionsTheme: const PageTransitionsTheme(
-          builders: <TargetPlatform, PageTransitionsBuilder>{
-            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
-            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
-          },
-        ),
-      ),
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.system,
-      home: FolderPage(),
-    );
+    return DynamicTheme(
+        defaultBrightness: Brightness.light,
+        data: (brightness) {
+          return ThemeData(
+            brightness: brightness,
+            primarySwatch: Colors.blue,
+            primaryColor: Color(0xff1995AD),
+            scaffoldBackgroundColor: Color(0xffF1F1F2),
+            floatingActionButtonTheme: FloatingActionButtonThemeData(
+                backgroundColor: Color(0xff1995AD), // TODO
+                foregroundColor: getWhiteOrBlack(Color(0xff1995AD))),
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            pageTransitionsTheme: const PageTransitionsTheme(
+              builders: <TargetPlatform, PageTransitionsBuilder>{
+                TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+                TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+              },
+            ),
+          );
+        },
+        themedWidgetBuilder: (context, theme) {
+          return new MaterialApp(
+            title: 'Flutter Demo',
+            theme: theme,
+            home: FolderPage(),
+          );
+        });
   }
 }
