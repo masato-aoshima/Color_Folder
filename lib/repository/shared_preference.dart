a1849a291e054a3237c05232488a2faad21e372f
Aoc4456 <sizu44567789@gmail.com>
2021-02-21 11:02:31 +0900

    shared_preferences を整理

--- lib/repository/shared_preference.dart
+++ lib/repository/shared_preference.dart
@@ -1,13 +1,25 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesKey {
-  // ノートの並び順
-  static const keyOrderOfNotes = 'order_of_notes';
-
   // アプリのテーマカラー
   static const keyThemeColor = 'theme_color';
+
+  // ノートの並び順
+  static const keyOrderOfNotes = 'order_of_notes';
 }
 
+///
+/// アプリのテーマカラー
+///
+Future<String> getThemeColorString() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getString(SharedPreferencesKey.keyThemeColor) ??
+      '0xff1995AD'; // デフォルトのテーマカラーをここで決める
+}
+
+///
+/// メモの並び順
+///
 List<String> orderOfNotesList = [
   'text ASC',
   'text DESC',
@@ -25,10 +37,4 @@ Future<String> getOrderOfNotesSetting() async {
 void saveOrderOfNotesSetting(String setting) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   prefs.setString(SharedPreferencesKey.keyOrderOfNotes, setting);
-}
-
-Future<String> getThemeColorString() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  return prefs.getString(SharedPreferencesKey.keyThemeColor) ??
-      '0xff1995AD'; // デフォルトのテーマカラーをここで決める
 }
