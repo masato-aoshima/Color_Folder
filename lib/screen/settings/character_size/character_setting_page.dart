b030fe07a2fd473f4f1221fb5f2d7c63edbc7ca3
Aoc4456 <sizu44567789@gmail.com>
2021-02-25 22:22:23 +0900

    縦横回転可能なレイアウトにする

--- lib/screen/settings/character_size/character_setting_page.dart
+++ lib/screen/settings/character_size/character_setting_page.dart
@@ -1,28 +1,108 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/all.dart';
-import 'package:sort_note/screen/settings/character_size/character_setting_model.dart';
 import 'package:sort_note/util/color.dart';
 
-final characterProvider =
-    ChangeNotifierProvider((ref) => CharacterSettingModel());
+class CharacterSettingPage extends StatefulWidget {
+  @override
+  _CharacterSettingPageState createState() => _CharacterSettingPageState();
+}
 
-class CharacterSettingPage extends HookWidget {
+class _CharacterSettingPageState extends State<CharacterSettingPage> {
+  double _value = 20;
+
+  void _changeSlider(double e) => setState(() {
+        _value = e;
+      });
+
   @override
   Widget build(BuildContext context) {
-    final provider = useProvider(characterProvider);
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          '文字の設定',
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: getWhiteOrBlackByThemeColor(context)),
+        appBar: AppBar(
+          title: Text(
+            '文字の設定',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: getWhiteOrBlackByThemeColor(context)),
+          ),
         ),
-      ),
-      body: Center(child: Text('文字の設定')),
-    );
+        body: OrientationBuilder(builder: (context, orientation) {
+          var orientation = MediaQuery.of(context).orientation;
+          return Flex(
+            direction: orientation == Orientation.portrait
+                ? Axis.vertical
+                : Axis.horizontal,
+            children: [
+              Flexible(
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Spacer(
+                        flex: 1,
+                      ),
+                      Container(
+                        color: Colors.blue[50],
+                        child: Column(
+                          children: <Widget>[
+                            Center(
+                                child: Text("文字の大きさ：$_value",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20))),
+                            CupertinoSlider(
+                              min: 5,
+                              max: 40,
+                              value: _value,
+                              activeColor: Colors.blue,
+                              divisions: 10,
+                              onChanged: _changeSlider,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        color: Colors.blue[200],
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Center(
+                                child: Text(
+                              "行間の大きさ：$_value",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            )),
+                            CupertinoSlider(
+                              min: 5,
+                              max: 40,
+                              value: _value,
+                              activeColor: Colors.blue,
+                              divisions: 10,
+                              onChanged: _changeSlider,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Spacer(
+                        flex: 3,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  color: Colors.red,
+                  child: Text(
+                      '文字のサイズを変更できます文字のサイズを変更できます文字のサイズを変更できます文字のサイズを変更できます文字のサイズを変更できます'),
+                ),
+              )
+            ],
+          );
+        }));
   }
 }
